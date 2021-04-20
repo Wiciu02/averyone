f02ba49a6921462b6256a1e151058ae8af94f998
Wiciu02 <wiktorbogacki@invisionapp.pl>
2021-04-20 18:25:40 +0200

    add new files

--- public/client.js
+++ public/client.js
@@ -149,6 +149,22 @@ y: touchEvent.touches[0].clientY - rect.top
 };
 }
 
+document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+
 canvas.addEventListener("mousedown", (e) => {
     mousePressed = true;
     draw(e);
