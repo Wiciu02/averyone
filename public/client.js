a74f21581439fa2b881e87d32a448e5014a6e7a7
Wiciu02 <75663814+Wiciu02@users.noreply.github.com>
2021-04-22 17:58:11 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -78,6 +78,21 @@ pencil.addEventListener('click', function(e) {
     lineWidth = 5;
 });
 
+// Get the container element
+var btnContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
 const marker = document.getElementById('prostokat');
 
 marker.addEventListener('click', function(e) {
