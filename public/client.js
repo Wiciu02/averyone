64b22447e809b566db7c2e29977b62aa774fc3ad
Wiciu02 <75663814+Wiciu02@users.noreply.github.com>
2021-04-22 13:09:31 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -137,87 +137,53 @@ document.addEventListener("mouseup", (e) => {
     lastPos = null;
 });
 
-var drawer = {
-    isDrawing: false,
-    touchstart: function (coors) {
-        context.beginPath();
-        context.moveTo(coors.x, coors.y);
-        this.isDrawing = true;
-    },
-    touchmove: function (coors) {
-        if (this.isDrawing) {
-            context.lineTo(coors.x, coors.y);
-            context.stroke();
-        }
-    },
-    touchend: function (coors) {
-        if (this.isDrawing) {
-            this.touchmove(coors);
-            this.isDrawing = false;
-        }
-    }
-};
-// create a function to pass touch events and coordinates to drawer
-function draw(event) { 
-    var type = null;
-    // map mouse events to touch events
-    switch(event.type){
-        case "mousedown":
-                event.touches = [];
-                event.touches[0] = { 
-                    pageX: event.pageX,
-                    pageY: event.pageY
-                };
-                type = "touchstart";                  
-        break;
-        case "mousemove":                
-                event.touches = [];
-                event.touches[0] = { 
-                    pageX: event.pageX,
-                    pageY: event.pageY
-                };
-                type = "touchmove";                
-        break;
-        case "mouseup":                
-                event.touches = [];
-                event.touches[0] = { 
-                    pageX: event.pageX,
-                    pageY: event.pageY
-                };
-                type = "touchend";
-        break;
-    }        
-
-    // touchend clear the touches[0], so we need to use changedTouches[0]
-    var coors;
-    if(event.type === "touchend") {
-        coors = {
-            x: event.changedTouches[0].pageX,
-            y: event.changedTouches[0].pageY
-        };
-    }
-    else {
-        // get the touch coordinates
-        coors = {
-            x: event.touches[0].pageX,
-            y: event.touches[0].pageY
-        };
-    }
-    type = type || event.type
-    // pass the coordinates to the appropriate handler
-    drawer[type](coors);
+function preventDefault(e) {
+    e.preventDefault();
 }
-var touchAvailable = ('createTouch' in document) || ('ontouchstart' in window);
+function disableScroll() {
+    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+function enableScroll() {
+    document.body.removeEventListener('touchmove', preventDefault);
+}
 
+var drawer = {
+   isDrawing: false,
+   touchstart: function (coors) {
+      ctx.beginPath();
+      ctx.moveTo(coors.x, coors.y);
+      this.isDrawing = true;
+      disableScroll(); // add for new iOS support
+   },
+   touchmove: function (coors) {
+      if (this.isDrawing) {
+         ctx.lineTo(coors.x, coors.y);
+         ctx.stroke();
+      }
+   },
+   touchend: function (coors) {
+      if (this.isDrawing) {
+         this.touchmove(coors);
+         this.isDrawing = false;
+      }
+      enableScroll(); // add for new iOS support
+   }
+};
 
-if(touchAvailable){
-    canvas.addEventListener('touchstart', draw, false);
-    canvas.addEventListener('touchmove', draw, false);
-    canvas.addEventListener('touchend', draw, false);        
+var touchAvailable = ('createTouch' in document) || ('onstarttouch' in window);
+
+if (touchAvailable) {
+   canvas.addEventListener('touchstart', draw, false);
+   canvas.addEventListener('touchmove', draw, false);
+   canvas.addEventListener('touchend', draw, false);
+} else {
+   canvas.addEventListener('mousedown', draw, false);
+   canvas.addEventListener('mousemove', draw, false);
+   canvas.addEventListener('mouseup', draw, false);
 }
 
 document.body.addEventListener('touchmove', function (event) {
-    event.preventDefault();
+   event.preventDefault();
 }, false);
 
 document.getElementById("clearBtn").addEventListener("click", () => {
