f847fc53552e9a898e7bb2be06eb1c00695948b0
Wiciu02 <75663814+Wiciu02@users.noreply.github.com>
2021-04-22 13:05:09 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -137,6 +137,27 @@ document.addEventListener("mouseup", (e) => {
     lastPos = null;
 });
 
+var drawer = {
+    isDrawing: false,
+    touchstart: function (coors) {
+        context.beginPath();
+        context.moveTo(coors.x, coors.y);
+        this.isDrawing = true;
+    },
+    touchmove: function (coors) {
+        if (this.isDrawing) {
+            context.lineTo(coors.x, coors.y);
+            context.stroke();
+        }
+    },
+    touchend: function (coors) {
+        if (this.isDrawing) {
+            this.touchmove(coors);
+            this.isDrawing = false;
+        }
+    }
+};
+// create a function to pass touch events and coordinates to drawer
 function draw(event) { 
     var type = null;
     // map mouse events to touch events
@@ -165,7 +186,39 @@ function draw(event) {
                 };
                 type = "touchend";
         break;
-    }  
+    }        
+
+    // touchend clear the touches[0], so we need to use changedTouches[0]
+    var coors;
+    if(event.type === "touchend") {
+        coors = {
+            x: event.changedTouches[0].pageX,
+            y: event.changedTouches[0].pageY
+        };
+    }
+    else {
+        // get the touch coordinates
+        coors = {
+            x: event.touches[0].pageX,
+            y: event.touches[0].pageY
+        };
+    }
+    type = type || event.type
+    // pass the coordinates to the appropriate handler
+    drawer[type](coors);
+}
+var touchAvailable = ('createTouch' in document) || ('ontouchstart' in window);
+
+
+if(touchAvailable){
+    canvas.addEventListener('touchstart', draw, false);
+    canvas.addEventListener('touchmove', draw, false);
+    canvas.addEventListener('touchend', draw, false);        
+}
+
+document.body.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+}, false);
 
 document.getElementById("clearBtn").addEventListener("click", () => {
     socket.emit("clearCanvas");
